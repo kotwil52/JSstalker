7b4434faf177c95f79fe9d3de18f92fbf914232c
kotwil52 <35931256+kotwil52@users.noreply.github.com>
2019-01-04 12:20:09 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var name = prompt("what is your name");
+var lastname = prompt("What is your lastanem");
+var age = prompt("What is your age");
+
+console.log(name + " " + lastname);
+console.log("ma lat " + age);
